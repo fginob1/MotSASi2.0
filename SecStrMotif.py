7a3bc6968fb532436dec5fc0ec063d4f76a24e76
Franco Gino Brunello <fgbrunello@gmail.com>
2025-03-23 20:30:22 -0300

    Renamed SecStrMotif_cluster.py to SecStrMotif.py

--- SecStrMotif.py
+++ SecStrMotif.py
@@ -238,16 +238,16 @@ if __name__ == "__main__":
     parser.add_argument("Motif_name", action="store", help = "Name of the motif, e.g. DOC_MAPK_JIP1_4")
     parser.add_argument("Motif_re", action="store", help = "Motif regular expression, e.g. [RK]P[^P][^P]L.[LIVMF]")
     parser.add_argument("Motif_label", action="store", help = "Motif regular expression for plotting, e.g. RK.P.^P.^P.L.x.LIVMF")
-    parser.add_argument("-fasta", "--sp_h_fasta", action="store", help = "file with fasta entries for SwissProt", default="/grupos/Marce/estructural/databases/UniProt/uniprot_sprot_h.fasta")
-    parser.add_argument("-xml", "--sp_h_xml", action="store", help = "file with xml entries for SwissProt", default="/grupos/Marce/estructural/databases/UniProt/uniprot_sprot_h.xml")
-    parser.add_argument("-list", "--sp_h_list", action="store", help = "file with all UniProtIDs in SwissProt", default="/grupos/Marce/estructural/databases/UniProt/uniprot_sprot_h.list")
-    parser.add_argument("-elm_inst", "--elm_instances", action="store", help = "ELM Instances database where look for motifs", default="/grupos/Marce/estructural/databases/ELM/elm_instances.tsv")
-    parser.add_argument("-pdb_str", "--pdb_structures", action="store", help = "PDB database with all crystals", default="/grupos/Marce/estructural/databases/PDB/zipped/")
-    parser.add_argument("-sec_str", "--secondary_structures", action="store", help = "folder with secondary structure predictions", default="/grupos/Marce/estructural/databases/secondary_structures/")
-    parser.add_argument("-af_human", "--alphafold_human", action="store", help = "alphafold for the human proteome", default="/grupos/Marce/estructural/databases/AlphaFold2/UP000005640_9606_HUMAN_v4/")
-    parser.add_argument("-scratch", "--scratch_path", action="store", help = "scratch secondary structure predictor path", default="/grupos/Marce/estructural/tools/scratch/SCRATCH-1D_1.3/bin/")
-    parser.add_argument("-tmp", "--tmp_path", action="store", help = "temporary folder", default="/grupos/Marce/estructural/motsasi/tmp/")
-    parser.add_argument("-cwd", "--cwd", action="store", help = "current working directory path", default="/grupos/Marce/estructural/motsasi/")
+    parser.add_argument("-fasta", "--sp_h_fasta", action="store", help = "file with fasta entries for SwissProt", default="./UniProt/uniprot_sprot_h.fasta")
+    parser.add_argument("-xml", "--sp_h_xml", action="store", help = "file with xml entries for SwissProt", default="./UniProt/uniprot_sprot_h.xml")
+    parser.add_argument("-list", "--sp_h_list", action="store", help = "file with all UniProtIDs in SwissProt", default="./UniProt/uniprot_sprot_h.list")
+    parser.add_argument("-elm_inst", "--elm_instances", action="store", help = "ELM Instances database where look for motifs", default="./ELM/elm_instances.tsv")
+    parser.add_argument("-pdb_str", "--pdb_structures", action="store", help = "PDB database with all crystals", default="./PDB/zipped/")
+    parser.add_argument("-sec_str", "--secondary_structures", action="store", help = "folder with secondary structure predictions", default="./secondary_structures/")
+    parser.add_argument("-af_human", "--alphafold_human", action="store", help = "alphafold for the human proteome", default="./AlphaFold2_human_proteome/UP000005640_9606_HUMAN_v4/")
+    parser.add_argument("-scratch", "--scratch_path", action="store", help = "scratch secondary structure predictor path", default="./scratch/SCRATCH-1D_1.3/bin/")
+    parser.add_argument("-tmp", "--tmp_path", action="store", help = "temporary folder", default="./tmp/")
+    parser.add_argument("-cwd", "--cwd", action="store", help = "current working directory path", default="./")
 
     args = parser.parse_args()
     
@@ -257,10 +257,10 @@ if __name__ == "__main__":
     if not os.path.exists(f"{args.tmp_path}"):
         os.system(f"mkdir {args.tmp_path}")
 
-    from MotifSearcher_cluster import HumanProteomeSearch
+    from MotifSearcher import HumanProteomeSearch
     screening_proteoma = HumanProteomeSearch(args.Motif_name, args.Motif_re, args.sp_h_fasta, args.sp_h_xml, args.sp_h_list)
 
-    from positive_ctrl_cluster import positive_ctrl
+    from positive_ctrl import positive_ctrl
     positive_ctrl(screening_proteoma, args.Motif_name, args.pdb_structures, args.elm_instances)
 
     true_positives = [match for match in screening_proteoma if match.ELM_Positive == True]
